244335bf6a9835d03caa156d564bab8f10e5c039
richie535 <37631780+richie535@users.noreply.github.com>
2023-05-19 15:51:29 -0400

    Delete Website/WebProject directory

--- Website/WebProject/View/menu.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset( $_SESSION['ON']))
-    echo "<a href='index.php?choice=logoff'> <span class='button'>LOGOFF</span></a>";
-else
-    echo "<a href='index.php'> <span class='button'>LOGIN</span></a>";
-    echo "<a href='index.php?choice=products'> <span class='button'>PRODUCTS</span></a>";
-    echo "<a href='index.php?choice=contact'> <span class='button'>CONTACT</span></a>";
-    echo "<a href='index.php?choice=about'><span class='button'>ABOUT</span></a>";
-?>
